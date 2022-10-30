fd9791904b28ded895be482bb6a7765ef0a6e37f
OlegBakulin <oleg_vampiriys@mail.ru>
2022-10-30 11:30:26 +0300

    Стреляет в правильном направлении

--- Assets/Scripts/Gameplay/Shooting/FrontalLaserController.cs
+++ Assets/Scripts/Gameplay/Shooting/FrontalLaserController.cs
@@ -1,18 +1,24 @@
+using Gameplay.Mechanics.Timer;
 using Scriptables.Modules;
 using System.Collections;
 using System.Collections.Generic;
-using System.Timers;
 using UnityEngine;
 
 namespace Gameplay.Shooting
 {
+    [RequireComponent(typeof(LineRenderer))]
     public class FrontalLaserController : FrontalTurretController
     {
         private readonly LaserWeaponConfig _weaponConfig;
         private LineRenderer _line;
+        private LineRenderer _laser;
         private Transform _playerTransform;
+        private GameObject _gun;
+        private Vector3 _endLaserPosition;
+
         public FrontalLaserController(TurretModuleConfig config, Transform gunPointParentTransform) : base(config, gunPointParentTransform)
         {
+            _gun = gunPointParentTransform.GetChild(0).gameObject;
             var laserConfig = config.SpecificWeapon as LaserWeaponConfig;
             _weaponConfig = laserConfig
                 ? laserConfig
@@ -28,22 +34,38 @@ namespace Gameplay.Shooting
             } 
 
             FireLaserProjectiles(_weaponConfig.MaxActiveTime_M, _weaponConfig.Long_Dlina);
-            CooldownTimer.Start();
+            //CooldownTimer.Start();
         }
 
         private void FireLaserProjectiles(float activeTime, float liserLong)
         {
-            _line = _playerTransform.GetComponent<LineRenderer>();
-            Time timerDamage = new Time();
-            timerDamage = activeTime * _weaponConfig.Koefficient_X;
-            var projectile = ProjectileFactory.CreateProjectile();
+            if (_laser) GameObject.Destroy(_laser.gameObject);
+            _endLaserPosition = (_gun.transform.position + _gun.gameObject.transform.TransformDirection(_gun.transform.localPosition + new Vector3(0, liserLong, 0))); //new Vector3 (_gun.transform.localPosition.x * _playerTransform.rotation.z, (_gun.transform.localPosition.y + liserLong) * _playerTransform.rotation.z, 0));
+            _line = _weaponConfig.LaserProjectile.Prefab.GetComponent<LineRenderer>();
+            _line.SetPosition(0, _gun.transform.position);
+            //ProjectileController projectile = ProjectileFactory.CreateProjectile();
+            _laser = GameObject.Instantiate(_line, _gun.transform.position, _gun.transform.rotation);
             RaycastHit hit;
-            if (Physics.Raycast(_playerTransform.position, Vector3.up, out hit, 10000f))
+            if (Physics.Raycast(_endLaserPosition, _gun.transform.position, out hit, liserLong))
             {
                 _line.SetPosition(1, hit.point);
             }
+            else
+            {
+                _line.SetPosition(1, _endLaserPosition);
+            }
+            /*
+            
+            
+            
+            _line.SetPosition(1, _gun.transform.parent.localPosition);
+            _line.SetPosition(0, _line.GetPosition(0) + new Vector3(0, 10,0));//_gun.transform.localPosition);
+            
+            
+                       
+                        */
 
-            AddController(projectile);
+            //AddController(projectile);
         }
     }
 }
--- Assets/Scripts/Gameplay/Shooting/LaserWeaponConfig.cs
+++ Assets/Scripts/Gameplay/Shooting/LaserWeaponConfig.cs
@@ -7,8 +7,8 @@ namespace Gameplay.Shooting
     {
         [field: SerializeField] public ProjectileConfig LaserProjectile { get; private set; }
         [field: SerializeField, Min(0.1f)] public float Long_Dlina { get; private set; } = 1f;
-        [field: SerializeField, Min(0.1f)] public float MaxActiveTime_M { get; private set; } = 1f;
-        [field: SerializeField, Min(0.1f)] public float ActiveTime_K { get; private set; } = 1f;
+        [field: SerializeField, Min(0.001f)] public float MaxActiveTime_M { get; private set; } = 0.001f;
+        [field: SerializeField, Min(0.001f)] public float ActiveTime_K { get; private set; } = 0.001f;
         [field: SerializeField, Range(0f, 1f)] public float Koefficient_X { get; private set; } = 1f;
     }
 }
