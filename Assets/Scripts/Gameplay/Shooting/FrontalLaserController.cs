8bd25db4ab9f6583aba9c54efdcf9a4634102fa6
OlegBakulin <oleg_vampiriys@mail.ru>
2022-11-12 14:28:43 +0300

    РАбочий постоянно вариант без КД

--- Assets/Scripts/Gameplay/Shooting/FrontalLaserController.cs
+++ Assets/Scripts/Gameplay/Shooting/FrontalLaserController.cs
@@ -1,4 +1,5 @@
 using Gameplay.Mechanics.Timer;
+using Gameplay.Player;
 using Scriptables.Modules;
 using System.Collections;
 using System.Collections.Generic;
@@ -6,66 +7,78 @@ using UnityEngine;
 
 namespace Gameplay.Shooting
 {
-    [RequireComponent(typeof(LineRenderer))]
     public class FrontalLaserController : FrontalTurretController
     {
         private readonly LaserWeaponConfig _weaponConfig;
-        private LineRenderer _line;
         private LineRenderer _laser;
         private Transform _playerTransform;
         private GameObject _gun;
         private Vector3 _endLaserPosition;
+        private Vector3 _startLaserPosition;
+        public RaycastHit2D _hit;
+        Ray2D _ray;
+        CircleCollider2D _collider;
+        GameObject _rectTransform;
+        LineRenderer _object;
+        Vector3 _angl;
+        Vector3 _pointObject;
+        PlayerController playerController;
 
         public FrontalLaserController(TurretModuleConfig config, Transform gunPointParentTransform) : base(config, gunPointParentTransform)
         {
-            _gun = gunPointParentTransform.GetChild(0).gameObject;
+            var _attackActive = gunPointParentTransform.gameObject.GetComponent<PlayerView>();
+            
             var laserConfig = config.SpecificWeapon as LaserWeaponConfig;
             _weaponConfig = laserConfig
                 ? laserConfig
                 : throw new System.Exception("Wrong config type was provided");
             _playerTransform = gunPointParentTransform;
+            _gun = _playerTransform.GetChild(0).gameObject;
+            _laser = _weaponConfig.Projectile.Prefab.gameObject.GetComponent<LineRenderer>();
+            _collider = _weaponConfig.Projectile.Prefab.gameObject.GetComponent<CircleCollider2D>();
+            _rectTransform = _weaponConfig.Projectile.Prefab.gameObject;
         }
-
+         
         public override void CommenceFiring()
         {
             if (IsOnCooldown)
             {
                 return;
-            } 
-
-            FireLaserProjectiles(_weaponConfig.MaxActiveTime_M, _weaponConfig.Long_Dlina);
-            //CooldownTimer.Start();
+            }
+            FireLaser();
         }
 
-        private void FireLaserProjectiles(float activeTime, float liserLong)
+        private void FireLaser()
         {
-            if (_laser) GameObject.Destroy(_laser.gameObject);
-            _endLaserPosition = (_gun.transform.position + _gun.gameObject.transform.TransformDirection(_gun.transform.localPosition + new Vector3(0, liserLong, 0))); //new Vector3 (_gun.transform.localPosition.x * _playerTransform.rotation.z, (_gun.transform.localPosition.y + liserLong) * _playerTransform.rotation.z, 0));
-            _line = _weaponConfig.LaserProjectile.Prefab.GetComponent<LineRenderer>();
-            _line.SetPosition(0, _gun.transform.position);
-            //ProjectileController projectile = ProjectileFactory.CreateProjectile();
-            _laser = GameObject.Instantiate(_line, _gun.transform.position, _gun.transform.rotation);
-            RaycastHit hit;
-            if (Physics.Raycast(_endLaserPosition, _gun.transform.position, out hit, liserLong))
-            {
-                _line.SetPosition(1, hit.point);
-            }
-            else
-            {
-                _line.SetPosition(1, _endLaserPosition);
-            }
-            /*
-            
-            
-            
-            _line.SetPosition(1, _gun.transform.parent.localPosition);
-            _line.SetPosition(0, _line.GetPosition(0) + new Vector3(0, 10,0));//_gun.transform.localPosition);
-            
-            
-                       
-                        */
+            //Проверка на старт
 
-            //AddController(projectile);
+            if (true)
+            {
+                _startLaserPosition = _gun.gameObject.transform.position;
+                _endLaserPosition = _gun.gameObject.transform.TransformPoint(Vector3.up * _weaponConfig.Long_Dlina);
+
+                _rectTransform.transform.rotation = new Quaternion(0, 0, _playerTransform.rotation.z, _playerTransform.rotation.w);
+
+                _laser.SetPosition(0, _startLaserPosition);
+                if ((_hit = Physics2D.Linecast(_startLaserPosition, _endLaserPosition)) && !(_hit.transform.gameObject.GetComponent<ProjectileView>()))
+                {
+                    UnityEngine.Debug.Log(_hit.collider.gameObject.name + " " + _hit.transform.position + " / " + _endLaserPosition.ToString() + " / " + _gun.transform.position.ToString() + " / " + _weaponConfig.Long_Dlina);
+
+                    _laser.SetPosition(1, _hit.point);
+                    _rectTransform.transform.localScale.Set(_rectTransform.transform.localScale.x, _hit.distance, 0);
+                }
+                else
+                {
+                    _laser.SetPosition(1, _endLaserPosition);
+                    _rectTransform.transform.localScale.Set(_rectTransform.transform.localScale.x, _weaponConfig.Long_Dlina, 0);
+                }
+                _collider.radius = _laser.widthMultiplier / 2;
+                _collider.offset = (Vector2)(_laser.GetPosition(1) - _laser.GetPosition(0));
+                var projectile = ProjectileFactory.CreateProjectile();
+                AddController(projectile);
+
+            }
+            //CooldownTimer.Start();
         }
     }
 }
